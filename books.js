5bdd09104772e0b902311d2871c7f77992dfc85a
GitP07 <priyeshpomburpekar30@gmail.com>
2023-09-04 17:04:53 +0530

    did mongoDB connection for all API

--- books.js
+++ books.js
@@ -1,256 +1,285 @@
 const express = require("express");
 const app  = express();
-var bodyParser = require('body-parser');
 
-const {MongoClient} = require("mongodb");
-const dataBase = "mongodb+srv://priyesh7:PG1ewPrnzZmK8HCa@cluster0.6jnltjp.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+const db_url = "mongodb+srv://priyesh7:7hCap9yPo88qiC7O@cluster0.6jnltjp.mongodb.net/BooksList?retryWrites=true&w=majority"
 
+const bodyParser = require("body-parser");
+
+const bookSchema = require("./bookSchema");
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+mongoose
+.connect(db_url,connectionParams)
+.then(()=>{
+    console.log("connection sucessful");
+})
+.catch((err)=>{
+    console.log("connection failed"+err);
+})
 
 app.use(bodyParser.json());
 
-var listBooks = [{"book_name":"The Alchemist","author_name":"Paulo Coelho","unique_id":1,"category":"Fiction","price":200},
-{"book_name":"Yellowface","author_name":"R. F. Kuang","unique_id":2,"category":"Fiction","price":180},
-{"book_name":"Dracula","author_name":"Bram Stoker","unique_id":3,"category":"Horror","price":350},
-{"book_name":"House of Leaves","author_name":"Mark Z. Danielewski","unique_id":4,"category":"Horror","price":250},
-{"book_name":"Ring","author_name":"Koji Suzuki","unique_id":5,"category":"Horror","price":150},
-{"book_name":"Bossypants","author_name":"Tina Fey","unique_id":6,"category":"Funny","price":120},
-{"book_name":"Bridget Jones's Diary","author_name":"Helen Fielding","unique_id":7,"category":"Funny","price":300},
-{"book_name":"The Silent Patient","author_name":"Alex Michaelides","unique_id":8,"category":"Thriller","price":400},
-{"book_name":"Gone Girl","author_name":"Gillian Flynn","unique_id":9,"category":"Mystery","price":400},
-{"book_name":"The Girl with the Dragon Tattoo","author_name":"Stieg Larsson","unique_id":10,"category":"Mystery","price":400}];
 
-
-let message = {};
-
+//Display all books data
 app.get("/", async function(req,res){
-    const client = new MongoClient(dataBase);
-
-    await client.connect().then(()=>{
-        console.log("DataBase Connection Sucessful");
-    }).catch((err)=>{
-        console.log("DataBase did not Connect");
-    });
-    res.send(JSON.stringify(listBooks)); 
+    const response = await bookSchema.find();
+    console.log(response[0]);
+    res.json(response);
 
 })
 
-app.get("/getBookByName/:title", function(req,res){
-    let bookname = req.params.title;
-    message = {err:"This Book Is Not Available In The Books List"};
-    for(var i = 0; i < listBooks.length; i++){
+//get book by name
+app.get("/getBookByName/:title", async function(req,res){
+    const enterName = req.params.title;
+    const booksList = await bookSchema.find();
 
-        console.log(listBooks[i].book_name.indexOf(bookname));
-        if(listBooks[i].book_name.indexOf(bookname) >= 0  ){
+    for(var i = 0; i < booksList.length; i++){
 
-            res.send(JSON.stringify(listBooks[i]))
-            return
+        console.log(booksList[i].book_name.indexOf(enterName));
+        if(booksList[i].book_name.indexOf(enterName) >= 0  ){
+
+           return res.json(booksList[i])
         }
       
     }
-    res.send(JSON.stringify(message));
+    res.json({message: "Book Is Not available"});
+
+   
 
 })
 
+//Edit book price
+app.post("/editBookPrice", async function(req,res){
 
-app.post("/editBookPrice",function(req,res){
     const id = req.body.unique_id;
-    let userArr = Object.keys(req.body);
-    let dataArr = Object.keys(listBooks[0]);
+    const newPrice = req.body.price
+    const bookList = await bookSchema.find();
+    let checkId = false;
+    const orgKeys = Object.keys(bookList[0].toObject());
+    const userKeys = Object.keys(req.body);
     let tempArr = [];
-
-    for (let i = 0; i < dataArr.length; i++) {
-        let validKey = userArr[i];
-        if(dataArr.indexOf(validKey) >= 0){
-            tempArr.push(userArr[i])
-        }
-        
-    }
     
-    if(userArr.length === tempArr.length){
-        for (let i = 0; i < listBooks.length; i++) {
-            if(listBooks[i].unique_id == id){
 
-                console.log(listBooks[i].book_name);
-                listBooks[i].price = req.body.price;
-                res.send(JSON.stringify(listBooks[i]));
-                return;
+    for (let i = 0; i < bookList.length; i++) {
+        let vaildArr = userKeys[i];
+        if(orgKeys.indexOf(vaildArr) >= 0){
+            tempArr.push(userKeys[i]);
+        }   
+    }
+
+    if(userKeys.length === tempArr.length){
+        for (let i = 0; i < bookList.length; i++) {
+            if (id === bookList[i].unique_id) {
+                checkId = true;
+                break;
             }
-        
+       
         }
-        message = {err:" This id does not exist"};
-        res.send(JSON.stringify(message));
+    
+        if(checkId){
+            const editPrice = await bookSchema.findOneAndUpdate({unique_id: id},{$set: {price: newPrice}},{new: true})
+            res.json(editPrice)
+        }
+        else{
+            res.json({message:"Book ID Not Found"});
+        }
     }
     else{
-        message = {err:"Please enter all and vaild keys of books"};
-        res.send(JSON.stringify(message));
+        res.json({message:"Please Enter All Vaild Keys"})
     }
+    
     
 })
 
 
 //Api For Updating Book Category 
-app.post("/editBookCategory", function(req,res){
-    const id = req.body.unique_id
-    let userArr = Object.keys(req.body);
-    let dataArr = Object.keys(listBooks[0]);
+app.post("/editBookCategory", async function(req,res){
+
+    const id = req.body.unique_id;
+    const catg = req.body.category;
+    const bookList = await bookSchema.find();
+    let checkId = false;
+    const orgKeys = Object.keys(bookList[0].toObject());
+    const userKeys = Object.keys(req.body);
     let tempArr = [];
+    
 
-    for (let i = 0; i < dataArr.length; i++) {
-
-        let validKey = userArr[i];
-        if(dataArr.indexOf(validKey) >= 0){
-
-            tempArr.push(userArr[i])
-        }
-        
+    for (let i = 0; i < bookList.length; i++) {
+        let vaildArr = userKeys[i];
+        if(orgKeys.indexOf(vaildArr) >= 0){
+            tempArr.push(userKeys[i]);
+        }   
     }
-    
-    
-    if(userArr.length === tempArr.length){
-        for(let i = 0; i < listBooks.length; i++){
-            if(id != null && listBooks[i].unique_id == id ){
 
-                listBooks[i].category = req.body.category;
-                res.send(JSON.stringify(listBooks[i]));
-                return;
+    if(userKeys.length === tempArr.length){
+        for (let i = 0; i < bookList.length; i++) {
+            if (id === bookList[i].unique_id) {
+                checkId = true;
+                break;
             }
+       
         }
-        message = {err:"This id does not exist"}
-        res.send(JSON.stringify(message));
+        if(checkId){
+            newCategory = await bookSchema.findOneAndUpdate({unique_id: id},{$set: {category: catg}},{new:true});
+            res.json(newCategory);
+        }
+        else{
+            res.json({message:"Book Id Not Found"})
+        }
     }
     else{
-        message = {err:"Please enter all and vaild keys of books"}
-        res.send(JSON.stringify(message));
+        res.json({message:"Please Enter All Valid Keys"})
     }
 
-})
+
+ })
 
 
 // Api For Updating Book Author Name
-app.post("/editBookAuthor", function(req,res){
-    const id = req.body.unique_id;
-    let userArr = Object.keys(req.body);
-    let dataArr = Object.keys(listBooks[0]);
-    let tempArr = [];
+app.post("/editBookAuthor", async function(req,res){
 
-    for (let i = 0; i < dataArr.length; i++) {
-        let validKey = userArr[i];
-        if(dataArr.indexOf(validKey) >= 0){
-            tempArr.push(userArr[i])
-        }
-        
+    const id = req.body.unique_id;
+    const authorName = req.body.author_name;
+    const bookList = await bookSchema.find();
+    let checkId = false;
+    const orgKeys = Object.keys(bookList[0].toObject());
+    const userKeys = Object.keys(req.body);
+    let tempArr = [];
+    
+
+    for (let i = 0; i < bookList.length; i++) {
+        let vaildArr = userKeys[i];
+        if(orgKeys.indexOf(vaildArr) >= 0){
+            tempArr.push(userKeys[i]);
+        }   
     }
 
-    if(userArr.length === tempArr.length){
-        for (let i = 0; i < listBooks.length; i++) {
-            
-            if(id != null && listBooks[i].unique_id == id){
-                    
-                listBooks[i].author_name = req.body.author_name;
-                res.send(JSON.stringify(listBooks[i]));
-                return;
+    if(userKeys.length === tempArr.length){
+        for (let i = 0; i < bookList.length; i++) {
+            if (id === bookList[i].unique_id) {
+                checkId = true;
+                break;
             }
-                
+       
         }
-    message = {err:"This Id Does Not Exist"};
-        res.send(JSON.stringify(message))
+        if(checkId){
+            const newAuthor = await bookSchema.findOneAndUpdate({unique_id: id},{$set: {author_name: authorName}},{new: true});
+            res.json(newAuthor);
+        }
+        else{
+            res.json({message:"Book Id Not Found"});
+        }
     }
     else{
-        message = {err:"Please Enter All Valid Keys"};
-        res.send(JSON.stringify(message));
-    } 
+        res.json({message:"Please Enter All Valid Keys"});
+    }
 
 })
 
 
 //Api to get all Book Category
-app.get("/getCategory", function(req,res){
+app.get("/getCategory", async function(req,res){
 
-    let catArr = [];
-    for(let i = 0; i < listBooks.length; i++){
-        catArr.push(listBooks[i].category);
+    const allCategory = await bookSchema.find({},'category');
+    
+    if(allCategory.length > 0){
+        res.json(allCategory);
+    }
+    else{
+        res.json({message:"There Are No Category Available"})
     }
 
-    res.send(catArr);
 })
 
 
 //Api to add new book in the booklist
-app.post("/addBook", function(req,res){
-    let newBook = req.body;
-    let userArr = Object.keys(req.body);
-    let dataArr = Object.keys(listBooks[0]);
+app.post("/addBook", async function(req,res){
+    const id = req.body.unique_id;
+    const bookList = await bookSchema.find();
+    let checkId = true;
+    const orgKeys = Object.keys(bookList[0].toObject());
+    const userKeys = Object.keys(req.body);
     let tempArr = [];
+    
 
-    for (let i = 0; i < dataArr.length; i++) {
-        let validKey = userArr[i];
-        if(dataArr.indexOf(validKey) >= 0){
-            tempArr.push(userArr[i])
-        }
-        
+    for (let i = 0; i < bookList.length; i++) {
+        let vaildArr = userKeys[i];
+        if(orgKeys.indexOf(vaildArr) >= 0){
+            tempArr.push(userKeys[i]);
+        }   
     }
 
-    var allbook = "";
-    if(userArr.length === tempArr.length){
-
-        for(let i = 0; i < listBooks.length; i++){
-
-            if(newBook.unique_id != null && listBooks[i].unique_id != newBook.unique_id ){
-                if(newBook.unique_id == listBooks.length+1){
-
-                    listBooks.push(newBook); 
-                    return;
-                }
-                else{
-                    let ID = listBooks.length+1
-                    message = {err:"new book id must be: ",id:ID}
-                    res.send(JSON.stringify(message));
-                    return;
-                }
-
-                
+    if(userKeys.length === tempArr.length){
+        for (let i = 0; i < bookList.length; i++) {
+            if (id === bookList[i].unique_id) {
+                checkId = false;
+                break;
             }
-            else{
-                message = {err:"ID Is Present"};
-                res.send(message);
-                return
-            }
-            
+   
         }
-        message = {mess:" New Book Added Successfully "};
-        res.send(JSON.stringify(message)+JSON.stringify(newBook)+JSON.stringify(listBooks));
 
+        if(checkId){
+            const newBook = new bookSchema(req.body);
+            await newBook.save();
+            const updatedBooksList = await bookSchema.find();
+            res.json({message:"updated Books List " , Books: updatedBooksList});
+
+        }
+        else{
+            res.json({message:"unique_id of books alresdy exist please change unique_id"});
+        }
     }
     else{
-        message = {err:"Please Enter Valid Keys Of Book To Add New Book To The List Of Books"};
-        res.send(message);
+        res.json({message:"Please Enter All Valid Keys"});
     }
-    
+            
 })
 
 
 //Api to delete book with their id 
-app.post("/deleteBook", function(req,res){
+app.post("/deleteBook", async function(req,res){
 
-    let id = req.body.unique_id;
-    let allbook = "";
+    const id = req.body.unique_id;
+    const bookList = await bookSchema.find();
+    let checkId = false;
+    const orgKeys = Object.keys(bookList[0].toObject());
+    const userKeys = Object.keys(req.body);
+    let tempArr = [];
+    
 
-    if (id <= listBooks.length && id > 0){
+    for (let i = 0; i < bookList.length; i++) {
+        let vaildArr = userKeys[i];
+        if(orgKeys.indexOf(vaildArr) >= 0){
+            tempArr.push(userKeys[i]);
+        }   
+    }
 
-        for(let j = 0; j < listBooks.length; j++){
-            if(id === listBooks[j].unique_id){
-                listBooks.splice(j,1);
-
+    if(userKeys.length === tempArr.length){
+        for (let i = 0; i < bookList.length; i++) {
+            if (id === bookList[i].unique_id) {
+                checkId = true;
+                break;
             }
-               
+       
         }
-        res.send(JSON.stringify(listBooks));
+        if(checkId){
+            await bookSchema.findOneAndDelete({unique_id:id});
+            const bookList = await bookSchema.find();
+            return res.json({message:"Book Deleted Sucessfuly", books: bookList})
+        }
+        else{
+            res.json({message:"Book Id Not Found"})
+        }
     }
     else{
-        message = {err:"ID Is Not Available In The Book List"};
-        res.send(message);
-    }  
+        res.json({message:"Please Enter All Valid Keys"})
+    }
 
+    
 
 })
 
